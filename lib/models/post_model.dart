f855d3fd621d838d9f1937ff09bdca29f7483ac5
Yasith Buddhima <yasithbuddhima@gmail.com>
2026-06-26 15:41:12 +0530

    feat: Enhance JSON serialization for Post subclasses and add error handling for unsupported post types

--- lib/models/post_model.dart
+++ lib/models/post_model.dart
@@ -5,6 +5,18 @@ abstract class Post {
 
   Post({required this.type, required this.timestamp, this.id});
   Map<String, dynamic> toJson(); // abstract method
+  factory Post.fromJson(Map<String, dynamic> json) {
+    switch (json['type']) {
+      case 'countdown':
+        return CountdownPost.fromJson(json);
+      case 'notice':
+        return NoticePost.fromJson(json);
+      case 'birthday':
+        return BirthdayPost.fromJson(json);
+      default:
+        throw ArgumentError('Unsupported post type: ${json['type']}');
+    }
+  }
 }
 
 class CountdownPost extends Post {
@@ -25,17 +37,18 @@ class CountdownPost extends Post {
       'id': id,
       'type': type,
       'message': message,
-      'endDate': endDate,
-      'timestamp': timestamp,
+      'endDate': endDate.toIso8601String(),
+      'timestamp': timestamp.toIso8601String(),
     };
   }
 
   // Create from JSON
   factory CountdownPost.fromJson(Map<String, dynamic> json) {
     return CountdownPost(
-      message: json['message'],
-      endDate: DateTime.parse(json['endDate']),
-      timestamp: DateTime.parse(json['timestamp']),
+      message: json['message'].toString(),
+      endDate: DateTime.parse(json['endDate'].toString()),
+      timestamp: DateTime.parse(json['timestamp'].toString()),
+      id: json['id']?.toString(),
     );
   }
 }
@@ -56,19 +69,22 @@ class NoticePost extends Post {
   @override
   Map<String, dynamic> toJson() {
     return {
+      'id': id,
+      'type': type,
       'title': title,
       'subtitle': subtitle ?? '',
       'content': content,
-      'timestamp': timestamp,
+      'timestamp': timestamp.toIso8601String(),
     };
   }
 
   factory NoticePost.fromJson(Map<String, dynamic> json) {
     return NoticePost(
-      title: json['title'],
-      subtitle: json['subtitle'] ?? '',
-      content: json['content'],
-      timestamp: DateTime.parse(json['timestamp']),
+      title: json['title'].toString(),
+      subtitle: json['subtitle']?.toString() ?? '',
+      content: json['content'].toString(),
+      timestamp: DateTime.parse(json['timestamp'].toString()),
+      id: json['id']?.toString(),
     );
   }
 }
@@ -87,14 +103,22 @@ class BirthdayPost extends Post {
 
   @override
   Map<String, dynamic> toJson() {
-    return {'name': name, 'birthday': birthday, 'photoUrl': photoURL ?? ''};
+    return {
+      'id': id,
+      'type': type,
+      'name': name,
+      'birthday': birthday.toIso8601String(),
+      'photoUrl': photoURL ?? '',
+      'timestamp': timestamp.toIso8601String(),
+    };
   }
 
   factory BirthdayPost.fromJson(Map<String, dynamic> json) {
     return BirthdayPost(
-      name: json['name'],
-      birthday: json['birthday'],
-      timestamp: DateTime.parse(json['timestamp']),
+      name: json['name'].toString(),
+      birthday: DateTime.parse(json['birthday'].toString()),
+      photoURL: json['photoUrl']?.toString(),
+      timestamp: DateTime.parse(json['timestamp'].toString()),
     );
   }
 }
